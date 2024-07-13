f43faef636f42d7b1475a2d4203a0562807d4415
Nodirbek20071 <nodirbeko2007@gmail.com>
2024-07-13 20:05:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <div> 
+         <div className="mx-auto bg-slate-300 w-20 h-20  rounded-full"></div>
+         <h1 className='text-4xl italic my-[15px]'>Headline</h1>
+         <p className='w-[200px] mx-auto my-[15px]'>Lorem ipsum dolor sit amet consectetur 
+           adipisicing elit. Ullam
+            quibusdam rerum alias.</p>
+      </div>
+
+  )
+}
+
+export default App
